e9d5ebec0a7a841ffe674f8d34a5cb2de031b3e3
prina patel <prina.patel@botreetechnologies.com>
2016-08-22 10:51:18 +0530

    New_app Scaffold

---
+++ app/models/product.rb
@@ -0,0 +1,22 @@
+class Product < ApplicationRecord
+   validates :title, :description, :image_url, presence: true
+   validates :price, numericality: {greater_than_or_equal_to: 0.01}
+   validates :image_url,allow_blank: true, format:{with: %r{\.(gif|png|jpg)\Z}i,message: 'must be a URL for GIF,JPG or PNG image.'}
+   has_many :line_items
+   before_destroy :ensure_not_referenced_by_any_line_item
+   private
+   def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+   end
+
+
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
+end
