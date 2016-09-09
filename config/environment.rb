90efe897b0306202f9d92709aace4020b52cc31b
prinapatel-btc <prina.patel@botreetechnologies.com>
2016-09-09 14:46:01 +0530

    environment.rb smpt comment

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
-config.action_mailer.delivery_method = :smtp
+#config.action_mailer.delivery_method = :smtp
 
 # Initialize the Rails application.
 Rails.application.initialize!
