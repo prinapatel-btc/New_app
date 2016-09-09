08449367f945e558de4f6ecfe954f74800bd7fc4
prinapatel-btc <prina.patel@botreetechnologies.com>
2016-09-09 14:40:07 +0530

    environment.rb add smtp

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,15 @@
 # Load the Rails application.
 require_relative 'application'
-
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "redkivi.com",
+      :user_name => "info@redkivi.com",
+      :password => "india@19",
+      :authentication => "plain",
+      :enable_starttls_auto => true
+  }
 # Initialize the Rails application.
 Rails.application.initialize!
 
