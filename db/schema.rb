d15d7ea5f0ea4f2da02aea74de39dd1aa22a4414
prinapatel-btc <prina.patel@botreetechnologies.com>
2016-09-09 16:02:30 +0530

    remember_me token migration destroy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909050305) do
+ActiveRecord::Schema.define(version: 20160909092647) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -53,7 +53,6 @@ ActiveRecord::Schema.define(version: 20160909050305) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "auth_token"
   end
 
 end
